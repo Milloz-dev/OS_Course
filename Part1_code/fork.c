fe6530a090c268467df9f7c2e73c2114697f6c6f
Milloz-dev <petter.star@telia.com>
2024-11-17 16:03:35 +0100

    uppgift 1,2 klar

--- Part1_code/fork.c
+++ Part1_code/fork.c
@@ -3,22 +3,56 @@
 
 int main(int argc, char **argv)
 {
-    pid_t pid;
+    pid_t pid1, pid2;
     unsigned i;
     unsigned niterations = 100;
-    pid = fork();
-    if (pid == 0) {
+
+    //first fork: First child process
+    pid1 = fork();
+
+    if (pid1 == 0){
+        //First child process
         for (i = 0; i < niterations; ++i)
             printf("A = %d, ", i);
-            printf("In child => Own pid : %d\n", getpid());
-            printf("In child => Parent's pid : %d\n", pid);
-     } else {
+        printf("In first child => Own pid : %d\n", getpid());
+        printf("In first child => Parent's pid : %d\n", getppid());
+        return 0;
+    }
+
+    //Second fork: second child process
+    pid2 = fork();
+
+    if(pid2 == 0) {
+
+        for(i = 0; i< niterations; ++i)
+            printf("C = %d, ", i);
+        printf("In second child => Own pid : %d\n", getpid());
+        printf("In second child => Parent's pid : %d\n", getppid());
+        return 0;
+    }
+
+    if(pid1 > 0 && pid2 > 0){
+        //parent process
         for (i = 0; i < niterations; ++i)
             printf("B = %d, ", i);
-            printf("In child => Own pid : %d\n", getpid());
-            printf("In child => Parent's pid : %d\n", pid);
+        printf("\nIn parent => First child's pid: %d\n", pid1);
+        printf("In parent => Second child's pid: %d\n", pid2);
+        return 0;
     }
-    printf("\n");
+
+    //pid = fork();
+    //if (pid == 0) {
+    //    for (i = 0; i < niterations; ++i)
+    //        printf("A = %d, ", i);
+    //        printf("In child => Own pid : %d\n", getpid());
+    //        printf("In child => Parent's pid : %d\n", pid);
+    // } else {
+    //    for (i = 0; i < niterations; ++i)
+    //        printf("B = %d, ", i);
+    //        printf("In child => Own pid : %d\n", getpid());
+    //        printf("In child => Parent's pid : %d\n", pid);
+    //}
+    //printf("\n");
     
     // }
     // else{
--- Part1_code/shmem.c
+++ Part1_code/shmem.c
@@ -3,16 +3,23 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <stdlib.h>
+#include <time.h>
 #define SHMSIZE 128
 #define SHM_R 0400
 #define SHM_W 0200
+#define BUFFER_SIZE 10
 
 int main(int argc, char **argv)
 {
 	struct shm_struct {
-		int buffer;
-		unsigned empty;
+		int buffer[BUFFER_SIZE];
+		unsigned empty_slots; //count of empty slots
+		unsigned full_slots; //count of full slots
+		unsigned head; //Head index for the cirular buffer
+		unsigned tail; //Tail index for the circular buffer
 	};
+
 	volatile struct shm_struct *shmp = NULL;
 	char *addr = NULL;
 	pid_t pid = -1;
@@ -22,30 +29,58 @@ int main(int argc, char **argv)
 	/* allocate a chunk of shared memory */
 	shmid = shmget(IPC_PRIVATE, SHMSIZE, IPC_CREAT | SHM_R | SHM_W);
 	shmp = (struct shm_struct *) shmat(shmid, addr, 0);
-	shmp->empty = 0;
+
+	/* Init the shared memory structure */
+	shmp->empty_slots = BUFFER_SIZE;
+	shmp->full_slots = 0;
+	shmp->head = 0;
+	shmp->tail = 0;
+
+	/* Seed the random number generator */
+    srand(time(NULL));
+
 	pid = fork();
+
 	if (pid != 0) {
 		/* here's the parent, acting as producer */
 		while (var1 < 100) {
 			/* write to shmem */
 			var1++;
-			while (shmp->empty == 1); /* busy wait until the buffer is empty */
+			
+			/* Wait until there's an empty slot */
+            while (shmp->empty_slots == 0);
 			printf("Sending %d\n", var1); fflush(stdout);
-			shmp->buffer = var1;
-			shmp->empty = 1;
+			shmp->buffer[shmp->head] = var1;
+			shmp->head = (shmp->head + 1) % BUFFER_SIZE;
+
+			/* Update the counters */
+			shmp->empty_slots--;
+			shmp->full_slots++;
+
+			/* Sleep for a random interval (0.1s to 0.5s) */
+            usleep((rand() % 400 + 100) * 1000);
 		}
+
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
 	} else {
 		/* here's the child, acting as consumer */
 		while (var2 < 100) {
 			/* read from shmem */
-			while (shmp->empty == 0); /* busy wait until there is something */
-			var2 = shmp->buffer;
-			shmp->empty = 0;
+			while (shmp->full_slots == 0); /* busy wait until there is something */
+			var2 = shmp->buffer[shmp->tail];
 			printf("Received %d\n", var2); fflush(stdout);
+			shmp->tail = (shmp->tail + 1) % BUFFER_SIZE;
+
+			/* Update the counters */
+			shmp->empty_slots++;
+			shmp->full_slots--;
+
+			/* Sleep for a random interval (0.2s to 2s) */
+            usleep((rand() % 1800 + 200) * 1000);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
 	}
+	return 0;
 }
