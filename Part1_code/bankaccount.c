51af4d40c432e806c857a14cb59527f7dd4fb5a3
Milloz-dev <petter.star@telia.com>
2024-11-22 14:03:29 +0100

    klar med task 7-9

--- Part1_code/bankaccount.c
+++ Part1_code/bankaccount.c
@@ -3,16 +3,24 @@
 #include <pthread.h>
 
 // Shared Variables
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-double bankAccountBalance = 0;
+double bankAccountBalance = 0.0;
 
+// Mutex
+pthread_mutex_t lock;
+
+// Deposit function
 void deposit(double amount) {
+    pthread_mutex_lock(&lock);
     bankAccountBalance += amount;
+    pthread_mutex_unlock(&lock);
 }
 
+// Withdraw function
 void withdraw(double amount) {
+    pthread_mutex_lock(&lock);
     bankAccountBalance -= amount;
-}
+    pthread_mutex_unlock(&lock);
+}   
 
 // utility function to identify even-odd numbers
 unsigned odd(unsigned long num) {
@@ -36,18 +44,40 @@ void* child(void* buf) {
 }
 
 int main(int argc, char** argv) {
-    pthread_t *children;
+    pthread_t *children; 
     unsigned long id = 0;
     unsigned long nThreads = 0;
-    if (argc > 1)
+
+    // Mutex innit
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        fprintf(stderr, "Error innit mutex\n");
+        return EXIT_FAILURE;
+    }
+
+    if (argc > 1){
         nThreads = atoi(argv[1]);
+        if(nThreads <= 0){
+            fprintf(stderr, "Number of threads must be possitiv");
+            return EXIT_FAILURE;
+        }
+    }else {
+        fprintf(stderr, "Usage: %s <numThreads>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     children = malloc( nThreads * sizeof(pthread_t) );
-    for (id = 1; id < nThreads; id++)
+    for (id = 1; id < nThreads; id++){
         pthread_create(&(children[id-1]), NULL, child, (void*)id);
+    }
+
     do1000Transactions(0); // main thread work (id=0)
-    for (id = 1; id < nThreads; id++)
+
+    for (id = 1; id < nThreads; id++){
         pthread_join(children[id-1], NULL);
+    }
+
     printf("\nThe final account balance with %lu threads is $%.2f.\n\n", nThreads, bankAccountBalance);
+
     free(children);
     pthread_mutex_destroy(&lock);
     return 0;
--- Part1_code/pthreadcreate3.c
+++ Part1_code/pthreadcreate3.c
@@ -5,37 +5,72 @@
 struct threadArgs {
 	unsigned int id;
 	unsigned int numThreads;
+	unsigned int squaredID; // Square of the thread ID = Child to Parent
 };
 
 void* child(void* params) {
 	struct threadArgs *args = (struct threadArgs*) params;
 	unsigned int childID = args->id;
-	unsigned int numThreads = args->numThreads;
-	printf("Greetings from child #%u of %u\n", childID, numThreads);
+
+	args->squaredID = childID * childID;
+	
+	printf("Greetings from child #%u of %u, SquaredID = %u\n", childID, args->numThreads, args->squaredID);
+
+	return NULL;
 }
 
 int main(int argc, char** argv) {
 	pthread_t* children; // dynamic array of child threads
 	struct threadArgs* args; // argument buffer
 	unsigned int numThreads = 0;
-	// get desired # of threads
-	if (argc > 1)
-		numThreads = atoi(argv[1]);
+
+	if (argc > 1) {
+    numThreads = atoi(argv[1]);
+	} else {
+    fprintf(stderr, "Usage: %s <numThreads>\n", argv[0]);
+    return EXIT_FAILURE;
+	}
+
+
+
+	// Allocate mermory for thread handles and argument structs
 	children = malloc(numThreads * sizeof(pthread_t)); // allocate array of handles
 	args = malloc(numThreads * sizeof(struct threadArgs)); // args vector
+
+	// Create Child threads
 	for (unsigned int id = 0; id < numThreads; id++) {
 		// create threads
 		args[id].id = id;
 		args[id].numThreads = numThreads;
-		pthread_create(&(children[id]), // our handle for the child
-			NULL, // attributes of the child
-			child, // the function it should run
-			(void*)&args[id]); // args to that function
+
+		// Create tread and check for errors
+		if (pthread_create(&(children[id]), NULL, child, (void*)&args[id]) != 0){
+			fprintf(stderr, "Error creating thread %u\n", id);
+			free(args);
+			free(children);
+			return EXIT_FAILURE;	
+		}
 	}
+
 	printf("I am the parent (main) thread.\n");
+
+	// Wait for child threads
 	for (unsigned int id = 0; id < numThreads; id++) {
-		pthread_join(children[id], NULL );
+		if (pthread_join(children[id], NULL) != 0) {
+            fprintf(stderr, "Error joining thread %u\n", id);
+            free(args);
+            free(children);
+            return EXIT_FAILURE;
+        }
 	}
+
+	 // Print the squared IDs collected from child threads
+    printf("Parent: Squared IDs from child threads:\n");
+    for (unsigned int id = 0; id < numThreads; id++) {
+        printf("Thread #%u: Squared ID = %u\n", id, args[id].squaredID);
+    }
+
+	// Free allocated memory
 	free(args); // deallocate args vector
 	free(children); // deallocate array
 	return 0;
